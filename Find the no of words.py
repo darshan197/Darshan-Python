503e3011b273111bebfd3a9c4150bf58a662c2e9
darshan197 <darshan.m@capillarytech.com>
2025-09-19 16:20:36 +0530

    Add files via upload

---
+++ Find the no of words.py
@@ -0,0 +1,5 @@
+sentence = input("Enter a sentence: ")
+char = input("Enter the character to count: ")
+
+count = sentence.count(char)
+print(f"The character '{char}' appears {count} times in the sentence.")
